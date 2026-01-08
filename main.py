f5a22f786e3e1999c071a9cef122b1cab3f13505
juliette <klicki@MacBook-Pro-de-juliette.local>
2026-01-08 17:14:12 +0100

    test

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Hello, backend is working"}
+
+class ChatRequest(BaseModel):
+    message: str
+
+@app.post("/chat")
+def chat(req: ChatRequest):
+    return {"answer": f"Tu as dit : {req.message}"}
